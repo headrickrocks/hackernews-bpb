0a833ffd9af1233ffa896ec2ded8a3fae6cdc628
csn-headrickrocks <brentbartholomew04@gmail.com>
2021-11-07 11:07:04 -0500

    Committing Pagination changes

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,68 +1,19 @@
-//This file is from Queries: Loading Links. It seems necessary to have in order to import LinkList in App.js.
-
-import React, { Component } from 'react'
+import React from 'react'
 import Link from './Link'
-import { Query } from 'react-apollo'
+import { useQuery } from 'react-apollo'
+import { useHistory } from 'react-router';
 import gql from 'graphql-tag'
-
-const NEW_VOTES_SUBSCRIPTION = gql`
-  subscription {
-    newVote {
-      node {
-        id
-        link {
-          id
-          url
-          description
-          createdAt
-          postedBy {
-            id
-            name
-          }
-          votes {
-            id
-            user {
-              id
-            }
-          }
-        }
-        user {
-          id
-        }
-      }
-    }
-  }
-`
-
-const NEW_LINKS_SUBSCRIPTION = gql`
-  subscription {
-    newLink {
-      node {
-        id
-        url
-        description
-        createdAt
-        postedBy {
-          id
-          name
-        }
-        votes {
-          id
-          user {
-            id
-          }
-        }
-      }
-    }
-  }
-`
-
+import { LINKS_PER_PAGE } from '../constants';
 export const FEED_QUERY = gql`
-  {
-    feed {
+  query FeedQuery(
+    $take: Int
+    $skip: Int
+    $orderBy: LinkOrderByInput
+  ) {
+    feed(take: $take, skip: $skip, orderBy: $orderBy) {
+      id
       links {
         id
-        createdAt
         url
         description
         postedBy {
@@ -75,73 +26,101 @@ export const FEED_QUERY = gql`
             id
           }
         }
+        createdAt
       }
+      count
     }
   }
-`
+`;
 
-class LinkList extends Component {
-  _updateCacheAfterVote = (store, createVote, linkId) => {
-    const data = store.readQuery({ query: FEED_QUERY })
-  
-    const votedLink = data.feed.links.find(link => link.id === linkId)
-    votedLink.votes = createVote.link.votes
-  
-    store.writeQuery({ query: FEED_QUERY, data })
+const getLinksToRender = (isNewPage, data) => {
+  if (isNewPage) {
+    return data.feed.links;
   }
+  const rankedLinks = data.feed.links.slice();
+  rankedLinks.sort(
+    (l1, l2) => l2.votes.length - l1.votes.length
+  );
+  return rankedLinks;
+};
 
-  _subscribeToNewLinks = subscribeToMore => {
-    subscribeToMore({
-      document: NEW_LINKS_SUBSCRIPTION,
-      updateQuery: (prev, { subscriptionData }) => {
-        if (!subscriptionData.data) return prev
-        const newLink = subscriptionData.data.newLink.node
-  
-        return Object.assign({}, prev, {
-          feed: {
-            links: [newLink, ...prev.feed.links],
-            count: prev.feed.links.length + 1,
-            __typename: prev.feed.__typename
-          }
-        })
-      }
-    })
-  }
+const getQueryVariables = (isNewPage, page) => {
+  const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0;
+  const take = isNewPage ? LINKS_PER_PAGE : 100;
+  const orderBy = { createdAt: 'desc' };
+  return { take, skip, orderBy };
+};
 
-  _subscribeToNewVotes = subscribeToMore => {
-    subscribeToMore({
-      document: NEW_VOTES_SUBSCRIPTION
-    })
-  }
+const LinkList = () => {
+  const history = useHistory();
+  const isNewPage = history.location.pathname.includes(
+    'new'
+  );
+  const pageIndexParams = history.location.pathname.split(
+    '/'
+  );
+  const page = parseInt(
+    pageIndexParams[pageIndexParams.length - 1]
+  );
 
-  render() {
-    return (
-      <Query query={FEED_QUERY}>
-        {({ loading, error, data, subscribeToMore }) => {
-          if (loading) return <div>Fetching</div>
-          if (error) return <div>Error</div>
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE : 0;
 
-          this._subscribeToNewLinks(subscribeToMore)
-          this._subscribeToNewVotes(subscribeToMore)
-    
-          const linksToRender = data.feed.links
-    
-          return (
-            <div>
-              {linksToRender.map((link, index) => (
-                <Link
-                  key={link.id}
-                  link={link}
-                  index={index}
-                  updateStoreAfterVote={this._updateCacheAfterVote}
-                />
-              ))}
+  const {
+    data,
+    loading,
+    error,
+  } = useQuery(FEED_QUERY, {
+    variables: getQueryVariables(isNewPage, page)
+  });
+
+
+  return (
+    <>
+      {loading && <p>Loading...</p>}
+      {error && <pre>{JSON.stringify(error, null, 2)}</pre>}
+      {data && (
+        <>
+          {getLinksToRender(isNewPage, data).map(
+            (link, index) => (
+              <Link
+                key={link.id}
+                link={link}
+                index={index + pageIndex}
+              />
+            )
+          )}
+          {isNewPage && (
+            <div className="flex ml4 mv3 gray">
+              <div
+                className="pointer mr2"
+                onClick={() => {
+                  if (page > 1) {
+                    history.push(`/new/${page - 1}`);
+                  }
+                }}
+              >
+                Previous
+              </div>
+              <div
+                className="pointer"
+                onClick={() => {
+                  if (
+                    page <=
+                    data.feed.count / LINKS_PER_PAGE
+                  ) {
+                    const nextPage = page + 1;
+                    history.push(`/new/${nextPage}`);
+                  }
+                }}
+              >
+                Next
+              </div>
             </div>
-          )
-        }}
-      </Query>
-    )
-  }
-}
+          )}
+        </>
+      )}
+    </>
+  );
+};
 
-export default LinkList
+export default LinkList;
